08f3f6b9f3a24ca022860c0f4c94a2acf620a17e
胡大海 <hudh23086@hundsun.com>
2018-05-14 16:46:37 +0800

    amthread工程创建

---
+++ study-thread/src/main/java/com/bigsea/study/studythread/StudyThreadApplication.java
@@ -0,0 +1,12 @@
+package com.bigsea.study.studythread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyThreadApplication.class, args);
+	}
+}
